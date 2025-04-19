ed8b24fcea290a5a6e9022412db44a90381f50c9
krishnagupta79658 <krishnagupta79658@gmail.com>
2025-04-19 23:47:37 +0530

    correcting spelling bugs

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -68,12 +68,12 @@ userSchema.methods.generateAccessToken=function(){
   expiresIn:process.env.ACCESS_TOKEN_EXPIRY
 })
 }
-userSchema.methods.generateRefreshToken=functon(){
+userSchema.methods.generateRefreshToken=function(){
   return jwt.sign({
     _id:this._id,
   },process.env.REFRESH_TOKEN_SECRET,
 {
-  expiresIn:process.env.REFRESF_TOKEN_EXPIRY
+  expiresIn:process.env.REFRESH_TOKEN_EXPIRY
 })
 }
 export const  User=mongoose.model("User",userSchema)
