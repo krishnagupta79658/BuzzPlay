95a727c09cb5b964a4816324739ed912feda3cad
krishnagupta79658 <krishnagupta79658@gmail.com>
2025-04-24 20:28:01 +0530

    add login controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
+const generateAccessAndRefreshTokens=async(userId){
+  try {
+    
+   const user =await User.findById(userId)
+   const accessToken=user.generateAccessToken()
+   const refreshToken=user.generateRefreshToken()
+   
+   user.refreshToken=refreshToken
+   await user.save({validateBeforeSave:false})
+   return {accessToken,refreshToken}
+  } catch (error) {
+    throw new ApiError(500,"Something went wrong while generating access and refresh token")
+  }
+}
+
 const registerUser= asyncHandler( async (req, res)=>{
     const {userName ,fullName ,email ,password}=req.body
    
@@ -24,7 +39,7 @@ const registerUser= asyncHandler( async (req, res)=>{
     } 
     
     const avatarLocalPath=req.files?.avatar[0]?.path
-   
+    
     //const coverImageLocalPath=req.files?.coverImage[0]?.path
     let coverImageLocalPath;
     if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0 ){
@@ -67,4 +82,42 @@ const registerUser= asyncHandler( async (req, res)=>{
 
 })
 
-export {registerUser}
+const loginUser= asyncHandler(async (req,res)=>{
+  const {username , email,password}=req.body
+  if(!username || !email){
+    throw new ApiError(400,"username or email is required")
+  }
+  const user=await User.findOne({
+    $or:[{username},{email}]
+  })
+  if(!user){
+    throw new ApiError(404,"user does not exist")
+  }
+  const isPasswordValid =await user.isPasswordCorrect(password)
+  if(!isPasswordCorrect){
+    throw new ApiError(401,"Invalid user credentials")
+  }
+
+  const {accessToken,refreshToken}=await generateAccessAndRefreshTokens(user._id)
+  //here we querying db but you can update user because here user does not contain the refreshToken depending upon you can bear this expensive cost
+  const loggedInUser=await User.findById(user._id).select("-password -refreshToken")
+ 
+  const options ={
+    httpOnly:true,
+    secure:true
+  }
+  return res.status(200)
+  .cookie("accessToken",accessToken,options)
+  .cookie("refreshToken",refreshToken,options)
+  .json(
+    new ApiResponse(
+ 200,{
+    user:loggedInUser,accessToken,refreshToken,
+ },
+ "user logged in successfully")
+  
+
+)
+})
+
+export {registerUser,loginUser}
